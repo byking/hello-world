7d5d23adb900ba999ced4df3e6c6562d3aac414f
byking <smithbyking@gmail.com>
2018-07-30 21:51:42 +0800

    ac binary tree inorder traversal

---
+++ medium/Binary_Tree_Inorder_Travesal/main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <gflags/gflags.h>
+#include <queue>
+#include <stack>
+
+DEFINE_string(tree1_preorder, "", "tree 1 preorder");
+DEFINE_string(tree1_inorder, "", "tree 1 inoreder");
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<string> split(string str, char a) {
+  vector<string> strvec;
+  string::size_type pos1, pos2;
+  pos2 = str.find(a);
+  pos1 = 0;
+  while (string::npos != pos2) {
+    strvec.push_back(str.substr(pos1, pos2 - pos1));
+    pos1 = pos2 + 1;
+    pos2 = str.find(a, pos1);
+  }
+  strvec.push_back(str.substr(pos1));
+  return strvec;
+}
+
+TreeNode *buildTreeByPreIn(const vector<string> *preorder, int pre_begin, int pre_end, 
+		           const vector<string> *inorder, int in_begin, int in_end) {
+  if (pre_begin == pre_end) return NULL;
+  if (in_begin == in_end) return NULL;
+
+  TreeNode *root = new TreeNode(stoi(preorder->at(pre_begin)));
+  
+  auto left_size = find(inorder->begin() + in_begin, 
+		         inorder->begin() + in_end, 
+			 preorder->at(pre_begin)) - inorder->begin() - in_begin;
+  
+  //printf("pre_begin:%d;pre_end:%d;in_begin:%d;in_end:%d;left_size:%d\n",
+  //       pre_begin,pre_end,in_begin,in_end,left_size);
+
+  root->left = buildTreeByPreIn(preorder, pre_begin + 1, pre_begin + left_size + 1, 
+		                inorder, in_begin, in_begin + left_size);
+  root->right = buildTreeByPreIn(preorder, pre_begin + left_size + 1, pre_end, 
+		                 inorder, in_begin + left_size + 1, in_end);
+  return root;
+}
+
+TreeNode *buildTreeByPreIn(const vector<string> *preorder,
+		           const vector<string> *inorder){
+  return buildTreeByPreIn(preorder, 0, preorder->size(), inorder, 0, inorder->size());
+}
+
+void bfsTree(TreeNode* root) {
+  cout << "bfsTree: ";
+  if (nullptr == root) {
+    return;
+  }
+  queue<TreeNode*> *my_queue = new queue<TreeNode*>();
+  my_queue->push(root);
+  while(!my_queue->empty()) {
+    TreeNode* node = my_queue->front();
+    cout << node->val << ",";
+    my_queue->pop();
+    if (nullptr != node->left) {
+      my_queue->push(node->left);
+    }
+    if (nullptr != node->right) {
+      my_queue->push(node->right);
+    }
+  }
+  cout << endl;
+  delete my_queue;
+  return;
+}
+
+void inorderTraversalRecursive(TreeNode* root) {
+  if (nullptr == root) return;
+  inorderTraversalRecursive(root->left);
+  cout << root->val << ";";
+  inorderTraversalRecursive(root->right);
+}
+
+vector<int> inorderTraversal(TreeNode* root) {
+  vector<int> res;
+  stack<TreeNode> inorder;
+  if (nullptr == root) {
+    return res;
+  }
+  TreeNode node = *root;
+  while (nullptr != node.left) {
+    inorder.push(node);
+    node = *(node.left);
+  }
+  inorder.push(node);
+  while (!inorder.empty()) {
+    TreeNode tmp = inorder.top();
+    res.push_back(tmp.val);
+    inorder.pop();
+    if (nullptr != tmp.right) {
+      TreeNode tmp_right = *(tmp.right);
+      while (nullptr != tmp_right.left) {
+        inorder.push(tmp_right);
+        tmp_right = *(tmp_right.left);
+      }
+      inorder.push(tmp_right);
+    }
+  }
+  return res;
+}
+
+int main(int argc, char* argv[]) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  cout << "get tree1 preorder: " << FLAGS_tree1_preorder << endl;
+  cout << "get tree1 inorder:  " << FLAGS_tree1_inorder << endl;
+
+  vector<string> tree1_preorder_vec = split(FLAGS_tree1_preorder, ',');  
+  vector<string> tree1_inorder_vec = split(FLAGS_tree1_inorder, ',');  
+
+  TreeNode *tree1_root = buildTreeByPreIn(&tree1_preorder_vec, &tree1_inorder_vec);
+   
+  bfsTree(tree1_root);
+  cout << "inorder traversal tree: ";
+  inorderTraversalRecursive(tree1_root);
+  cout << endl;
+  
+  cout << "inorder traversal tree(no recursive): ";
+  tree1_root = NULL;
+  vector<int> inorderTraversalRes = inorderTraversal(tree1_root); 
+  for(vector<int>::iterator it = inorderTraversalRes.begin(); it != inorderTraversalRes.end(); ++it) {
+    cout << *it;
+  }
+  cout << endl;
+  return 0;
+}
