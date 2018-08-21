fdedd7f3f21f51ded22abc32b1c13ebf1b23980e
byking <smithbyking@gmail.com>
2018-08-21 11:07:13 +0800

    ac binary search tree iterator

---
+++ medium/Binary_Search_Tree_Iterator/README.md
@@ -0,0 +1,14 @@
+# same tree 
+- build tree
+- print tree
+- test tree
+
+## build 
+- install gflags
+- sh build.sh
+
+## test:
+
+```bash
+./output/test_tree --tree1_preorder=10,5,4,6,12,11,15 --tree1_inorder=4,5,6,10,11,12,15
+```
---
+++ medium/Binary_Search_Tree_Iterator/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <gflags/gflags.h>
+#include <queue>
+#include <stack>
+
+DEFINE_string(tree1_preorder, "", "tree 1 preorder");
+DEFINE_string(tree1_inorder, "", "tree 1 inoreder");
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class BSTIterator {
+private:
+  stack<TreeNode*> _stack;
+  TreeNode* _cur_node;
+public:
+  BSTIterator(TreeNode* root) {
+    _cur_node = root;
+    while (nullptr != _cur_node) {
+      _stack.push(_cur_node);
+      _cur_node = _cur_node->left;
+    }    
+  }
+
+  bool hasNext() {
+    return !_stack.empty(); 
+  }
+
+  int next() {
+    int res; 
+    res = _stack.top()->val;
+    _cur_node = _stack.top()->right;
+    _stack.pop();
+    while (nullptr != _cur_node) {
+      _stack.push(_cur_node);
+      _cur_node = _cur_node->left;
+    }
+    return res;
+  }
+};
+
+vector<string> split(string str, char a) {
+  vector<string> strvec;
+  string::size_type pos1, pos2;
+  pos2 = str.find(a);
+  pos1 = 0;
+  while (string::npos != pos2) {
+    strvec.push_back(str.substr(pos1, pos2 - pos1));
+    pos1 = pos2 + 1;
+    pos2 = str.find(a, pos1);
+  }
+  strvec.push_back(str.substr(pos1));
+  return strvec;
+}
+
+TreeNode *buildTreeByPreIn(const vector<string> *preorder, int pre_begin, int pre_end, 
+		           const vector<string> *inorder, int in_begin, int in_end) {
+  if (pre_begin == pre_end) return NULL;
+  if (in_begin == in_end) return NULL;
+
+  TreeNode *root = new TreeNode(stoi(preorder->at(pre_begin)));
+  
+  auto left_size = find(inorder->begin() + in_begin, 
+		         inorder->begin() + in_end, 
+			 preorder->at(pre_begin)) - inorder->begin() - in_begin;
+  
+  //printf("pre_begin:%d;pre_end:%d;in_begin:%d;in_end:%d;left_size:%d\n",
+  //       pre_begin,pre_end,in_begin,in_end,left_size);
+
+  root->left = buildTreeByPreIn(preorder, pre_begin + 1, pre_begin + left_size + 1, 
+		                inorder, in_begin, in_begin + left_size);
+  root->right = buildTreeByPreIn(preorder, pre_begin + left_size + 1, pre_end, 
+		                 inorder, in_begin + left_size + 1, in_end);
+  return root;
+}
+
+TreeNode *buildTreeByPreIn(const vector<string> *preorder,
+		           const vector<string> *inorder){
+  return buildTreeByPreIn(preorder, 0, preorder->size(), inorder, 0, inorder->size());
+}
+
+void bfsTree(TreeNode* root) {
+  cout << "bfsTree: ";
+  if (nullptr == root) {
+    return;
+  }
+  queue<TreeNode*> *my_queue = new queue<TreeNode*>();
+  my_queue->push(root);
+  while(!my_queue->empty()) {
+    TreeNode* node = my_queue->front();
+    cout << node->val << ",";
+    my_queue->pop();
+    if (nullptr != node->left) {
+      my_queue->push(node->left);
+    }
+    if (nullptr != node->right) {
+      my_queue->push(node->right);
+    }
+  }
+  cout << endl;
+  delete my_queue;
+  return;
+}
+
+int main(int argc, char* argv[]) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  cout << "get tree1 preorder: " << FLAGS_tree1_preorder << endl;
+  cout << "get tree1 inorder:  " << FLAGS_tree1_inorder << endl;
+
+  vector<string> tree1_preorder_vec = split(FLAGS_tree1_preorder, ',');  
+  vector<string> tree1_inorder_vec = split(FLAGS_tree1_inorder, ',');  
+
+  TreeNode *tree1_root = buildTreeByPreIn(&tree1_preorder_vec, &tree1_inorder_vec);
+
+  bfsTree(tree1_root);
+
+  BSTIterator i = BSTIterator(tree1_root);
+  while (i.hasNext()) cout << i.next()<< ";";
+
+  return 0;
+}
